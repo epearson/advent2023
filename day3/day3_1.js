78809fca5b717412c34314b263c4776b422f15f9
Erik Pearson <smallrocksfloat@gmail.com>
2023-12-04 07:49:19 -0500

    day3 start

---
+++ day3/day3_1.js
@@ -0,0 +1,114 @@
+const fs = require('fs');
+const readline = require('readline');
+
+async function processLineByLine(file) {
+    const fileStream = fs.createReadStream(file);
+    var total = 0;
+  
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+    // Note: we use the crlfDelay option to recognize all instances of CR LF
+    // ('\r\n') in input.txt as a single line break.
+  
+    let yIndex = 0;
+
+    let parts = [];
+    let touchMap = {};
+    let lineArray = [];
+
+    for await (const line of rl) {
+      // Each line in input.txt will be successively available here as `line`.
+      console.log(`Line from file: ${line}`);
+      
+      lineArray.push(line);
+
+      //let p = line.split(/\D+/).filter(Boolean);
+
+      //parts = parts.concat(p);
+
+      //console.log(parts);
+
+      let symbols = Array.from(line.matchAll(/[^0-9\.]/gi));
+
+      for (let symbol of symbols) {
+        touchMap[(symbol.index + 1) + ',' + yIndex] = true;
+        touchMap[(symbol.index - 1) + ',' + yIndex] = true;
+        touchMap[(symbol.index + 1) + ',' + (yIndex + 1)] = true;
+        touchMap[(symbol.index - 1) + ',' + (yIndex - 1)] = true;
+        touchMap[(symbol.index + 1) + ',' + (yIndex - 1)] = true;
+        touchMap[(symbol.index - 1) + ',' + (yIndex + 1)] = true;
+        touchMap[symbol.index + ',' + (yIndex + 1)] = true;
+        touchMap[symbol.index + ',' + (yIndex - 1)] = true;
+      }
+
+      //console.log(symbols);
+
+      yIndex = yIndex + 1;
+
+    }
+
+    for (let i = 0; i < lineArray.length; i++) {
+        let line = lineArray[i];
+        console.log(line);
+
+        let numbers = Array.from(line.matchAll(/[0-9]/gi));
+
+      let prevNumIndex = 0;
+      let prevNumIsTouching = false;
+      let numberStr = '';
+      let isTouching = false;
+
+      for (let j = 0; j < numbers.length; j++) {
+        let number = numbers[j];
+        console.log(number);
+
+        if (prevNumIndex + 1 == number.index) {
+            numberStr = numberStr + number;
+        } else {
+            // if a new number and the last was touching, add to parts
+            if (isTouching) {
+                console.log('Adding to parts: ' + numberStr);
+                parts.push(parseInt(numberStr));
+            }
+
+
+            numberStr = number;
+            isTouching = false;
+            prevNumIsTouching = false;
+        }
+
+        //console.log('Number String: ' + numberStr);
+
+        if (!prevNumIsTouching) {
+            let xyIndex = number.index + ',' + i;
+
+            isTouching = touchMap[xyIndex] ? true : false;
+            prevNumIsTouching = isTouching;
+
+            /*if (isTouching) {
+                
+            }*/
+        } else {
+            //prevNumIsTouching = false; // reset
+        }
+
+        console.log(isTouching);
+
+        prevNumIndex = number.index;
+
+        // if last, and touching, add to parts
+        if (j == numbers.length - 1 && isTouching) {
+            parts.push(parseInt(numberStr));
+        }
+      }
+    }
+
+    console.log(parts);
+    //console.log(touchMap);
+    total = parts.reduce((x,y) => x + y);
+    console.log(total);
+  }
+  
+  processLineByLine('input-3-1.txt');
